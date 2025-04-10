6859717c6aaa9fb5ae1eeb23d6d9d581a38afda3
MFLS2023 <zc23@zua.edu.cn>
2025-04-10 20:40:40 +0800

    Update worker.js
    补充完整API代理

--- src/worker.js
+++ src/worker.js
@@ -51,8 +51,50 @@ const API_CONFIGS = {
     geminiproxypanel: {
     host: "gaoqilan-gemini-proxy-panel.hf.space",
     paths: ["/v1"],
-    description: "Gemini 代理池代理服务",
+    description: "Gemini API 代理池代理服务",
     logo: "💻"
+  },
+    qodo: {
+    host: "gaoqilan-qodo2api.hf.space",
+    paths: ["/v1"],
+    description: "Qodo 2 API 代理池代理服务",
+    logo: "🧨"
+  },
+    damser(grok) : {
+    host: "api.damser.xyz",
+    paths: ["/v1"],
+    description: "Grok API 代理池代理服务",
+    logo: "🎉"
+  },
+    openRouter(ds_gemini) : {
+    host: "openrouter.ai/api",
+    paths: ["/v1"],
+    description: "OpenRouter API 代理服务",
+    logo: "🎪"
+  },
+    chutes(ds) : {
+    host: "llm.chutes.ai",
+    paths: ["/v1"],
+    description: "Chutes API 代理服务",
+    logo: "🎇"
+  },
+    yunwu : {
+    host: "free.yunwu.ai",
+    paths: ["/v1"],
+    description: "Yunwu API 代理服务",
+    logo: "🎊"
+  },
+    genspark : {
+    host: "gs.aytsao.cn",
+    paths: ["/v1"],
+    description: "Dean API 代理服务",
+    logo: "🎏"
+  },
+    deepinfra(ds_claude) : {
+    host: "api.deepinfra.com/v1/openai/",
+    paths: ["/v1"],
+    description: "Deepinfra API 代理服务",
+    logo: "🎯"
   }
   };
 
